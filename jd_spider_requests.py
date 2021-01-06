0a4ad24b44f1b73be877dd049f59b6f441941bb7
mjzcng <mjzcng@126.com>
2021-01-06 15:49:18 +0800

    1

--- jd_spider_requests.py
+++ jd_spider_requests.py
@@ -275,7 +275,7 @@ class JdSeckill(object):
 
         # 初始化信息
         self.sku_id = global_config.getRaw('config', 'sku_id')
-        self.seckill_num = 2
+        self.seckill_num = 1
         self.seckill_init_info = dict()
         self.seckill_url = dict()
         self.seckill_order_data = dict()
@@ -329,7 +329,7 @@ class JdSeckill(object):
         self._seckill()
 
     @check_login
-    def seckill_by_proc_pool(self, work_count=5):
+    def seckill_by_proc_pool(self, work_count=1):
         """
         多进程进行抢购
         work_count：进程数量
